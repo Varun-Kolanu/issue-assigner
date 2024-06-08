56bb186e3fd81e9a718b51977fd3ff761271b750
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-06-09 00:31:30 +0530

    feat: add webhook POST route for marketplace

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import issue_comment_created from "./handlers/issue_comment_created.js";
 import issue_opened from "./handlers/issue_opened.js";
+import bodyParser from "body-parser";
 
 export default (app, { getRouter }) => {
   const router = getRouter("/");
+  router.use(bodyParser.json());
   router.get("/", (_, res) => {
     res.send("Welcome to Issue Assigner");
+  });
+
+  router.post("/webhook", (req, res) => {
+    console.log("Webhook from marketplace: ", req.body);
+    res.status(200).json({ success: "Webhook received successfully" });
   });
 
   // Issue opened by a user
